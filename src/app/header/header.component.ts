c1c7acaf3900e958df6f7c03584105dccdf21537
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2020-05-11 13:30:10 +0300

    Move header items to center.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,18 +11,17 @@ import {Router} from '@angular/router';
     <!-- menu -->
     <mat-toolbar color="primary">
       <mat-toolbar-row>
-        <h1>Harry Potter</h1>
         <span class="menu-spacer"></span>
-
         <div>
           <a mat-button [routerLink]="'characters'" routerLinkActive="active">Characters</a>
           <a mat-button [routerLink]="'houses'" routerLinkActive="active">Houses</a>
           <a mat-button [routerLink]="'spells'" routerLinkActive="active">Spells</a>
         </div>
+        <span class="menu-spacer"></span>
       </mat-toolbar-row>
 
     </mat-toolbar>`,
-  styles: [`.active { background-color: lightgray; } .pointer {cursor: pointer}`]
+  styles: [`.active { background-color: lightgray; } .pointer {cursor: pointer} .menu-spacer {flex-grow: 1}`]
 })
 export class HeaderComponent implements OnInit {
 
