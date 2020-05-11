2ca93632914bf535079ce978eb8d1b1f257ac7c4
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2020-05-11 13:40:17 +0300

    Make footer work better and move text to right corner.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,15 +5,18 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <footer class="footer">
     <div class="container">
-    <div class="content has-text-centered">
-      <p>
-        Made by Hanna Tuominen
-      </p>
-    </div>
+      <div class="content">
+        <p class="api-link">
+          Made by Hanna Tuominen
+        </p>
+        <p class="api-link">
+          This site uses the <a href="https://www.potterapi.com/">Potter API</a>
+        </p>
+      </div>
     </div>
     </footer>
   `,
-  styleUrls: []
+  styles: [`.menu-spacer {flex-grow: 1} .footer { background-color: #c2c3c4;} .api-link {margin: 0 10px 0 0; text-align: end}`]
 })
 export class FooterComponent implements OnInit {
 
